ea70fdcb599dcd9be2d8a3e26dbf7555fe988968
NEVA <neva.vka@yandex.ru>
2022-10-28 13:34:31 +0300

    Задание 41

--- Dz01/Program.cs
+++ Dz01/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿Console.Clear();
+
+Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int [] arr;
+arr = new int[size];
+for( int i = 0; i < arr.Length; i++)
+{
+    arr[i] = new Random().Next(-100,100);
+    Console.Write(arr[i] + " ");
+}
+Console.WriteLine();
+
+int count = 0;
+for( int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0) 
+    {
+        count++;
+    }
+}
+Console.WriteLine($"Положительных чисел = {count}");
